c91396871f77c5003e2488c72183f20ffe99c66c
galzart <zarinatazhieva9@mail.ru>
2025-01-26 17:23:09 +0500

    выполнена 4 задача 2 урока

---
+++ Task_02_04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год рождения:");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите месяц рождения:");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите день рождения:");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime birthDate = new DateTime(year, month, day);
+            DateTime currentDate = DateTime.Now;
+
+            int age = currentDate.Year - birthDate.Year;
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Вы являтесь совершеннолетним.");
+            }
+            else
+            {
+                Console.WriteLine("Вы не являтесь совершеннолетним.");
+            }
+        }
+    }
+}
+
+          
