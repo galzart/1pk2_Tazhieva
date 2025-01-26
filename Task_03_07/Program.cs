32a3a836179078581d928f82641d1b7d384503f3
galzart <zarinatazhieva9@mail.ru>
2025-01-26 17:39:08 +0500

    выполнена 7 задача 3 урока

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+        /*Написать программу, которая выводит 
+         * таблицу скорости(через каждые 0,5с) 
+         * свободно падающего тела v = g t ,
+         * где 2 g = 9,8 м/с2 – ускорение 
+         свободного падения*/
+
+        static void Main(string[] args)
+        {
+            double g = 9.8; double t;
+            Console.WriteLine("Введите ограничение времени");
+            while (!double.TryParse(Console.ReadLine(), out t))
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            Console.Clear();
+            Console.WriteLine("Время (с)  |  Скорость (м/с)");
+            Console.WriteLine("----------------------------");
+
+            for (double i = 0.5; i <= t; i += 0.5)
+            {
+                Console.WriteLine($"{i}         {i * g}");
+            }
+        }
+    }
+}
