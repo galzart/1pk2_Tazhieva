aa8ac9bf34a9e48189c409b7fc57d474ca239be4
galzart <zarinatazhieva9@mail.ru>
2025-01-26 17:40:57 +0500

    выполнена 8 задача 3 урока

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        /*Даны натуральные числа от 20 до 50. 
+         * Напечатать те из них, которые делятся на 3,
+        но не делятся на 5.*/
+
+        static void Main(string[] args)
+        {
+
+            int a = 20, b = 50;
+            while (b != a)
+            {
+                if (b % 3 == 0 & b % 5 != 0)
+                {
+                    Console.WriteLine(b);
+
+                }
+                --b;
+
+            }
+        }
+    }
+}
