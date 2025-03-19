1b286a7803966ecf8d8274d6836a351576602e66
galzart <zarinatazhieva9@mail.ru>
2025-03-19 22:05:24 +0500

    выплнена 3 задача 14 урока

---
+++ Task_14_03/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_14_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n;
+            Console.WriteLine("Введите число");
+
+            while (!long.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+
+            if (n < 0)   // Простая проверка без лишних условий
+            {
+                Console.WriteLine("Факториал числа определен только для положительных чисел");
+            }
+            else
+            {
+
+                Console.WriteLine("Факториал числа " + n + " = " + Factorial(n)); // Вывод без форматирования строк
+            }
+        }
+
+        // Метод без проверок внутри, новичок мог забыть обработать 0 или 1
+        static long Factorial(long a)
+        {
+            long factorial = 1;
+
+            // Цикл с лишней переменной
+            int i = 1;
+            while (i <= a) // Новичок мог использовать while вместо for
+            {
+                factorial = factorial * i;
+                i++; // Инкремент в теле цикла
+            }
+
+            return factorial;
+        }
+    }
+}
+
