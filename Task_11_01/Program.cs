48c8a00d75b3bc73414183fcd4b1cd3358c4cf23
galzart <zarinatazhieva9@mail.ru>
2025-03-19 17:30:41 +0500

    выполнена 1 задача 11 урока

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_11_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            /*Передача по значению: Напишите метод, который принимает два целых числа и меняет их
+местами. Проверьте, изменились ли значения переменных вне метода*/
+
+            Random rnd = new Random();
+
+            // Генерируем два случайных числа
+            int a = rnd.Next(1, 100);
+            int b = rnd.Next(1, 100);
+
+            Console.WriteLine($"До вызова метода:  a = {a}, b = {b}");
+
+            // Вызываем метод для обмена значений
+            Swap(a, b);
+
+            Console.WriteLine($"После вызова метода: a = {a}, b = {b}");
+        }
+
+        // Метод пытается поменять значения местами
+        static void Swap(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+
+            // Эти изменения останутся только внутри метода
+            Console.WriteLine($"Внутри метода:     x = {x}, y = {y}");
+        }
+    }
+}
