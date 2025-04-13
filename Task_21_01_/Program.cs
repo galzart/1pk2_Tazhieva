b1a4dd17f77af82fd314f56645f07851916dd79d
galzart <zarinatazhieva9@mail.ru>
2025-04-13 22:43:02 +0500

    выполнена 1 задача 21 урока

---
+++ Task_21_01_/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Task_21_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Создаем исходный список чисел
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // 2. Используем LINQ для фильтрации и преобразования списка
+            //    (а) Фильтруем список, оставляя только четные числа (number % 2 == 0)
+            //    (б) Умножаем каждое четное число на 10 (number * 10)
+            List<int> evenNumbersMultiplied = numbers
+                .Where(number => number % 2 == 0) // Фильтрация: выбираем только четные числа
+                .Select(number => number * 10)  // Преобразование: умножаем каждое число на 10
+                .ToList();                     // Преобразуем результат обратно в список
+
+            // 3. Выводим новый список на экран
+            Console.WriteLine("Исходный список: " + string.Join(", ", numbers)); // string.Join склеивает элементы списка в строку
+            Console.WriteLine("Список четных чисел, умноженных на 10: " + string.Join(", ", evenNumbersMultiplied));
+
+            Console.ReadKey(); // Чтобы консоль не закрылась сразу
+        }
+    }
+}
